e2d43fdc8047ac1f17d1055a7697dbf2234eb12f
ChickenNugz <cameronqe@gmail.com>
2024-03-21 11:25:48 -0600

    Added Docstrings

--- vignere cipher.py
+++ vignere cipher.py
@@ -1,38 +1,41 @@
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz., ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-letter_to_index = dict(zip(alphabet, range(len(alphabet))))
-index_to_letter = dict(zip(range(len(alphabet)), alphabet))
+letter_then_index = dict(zip(alphabet, range(len(alphabet))))
+index_then_letter = dict(zip(range(len(alphabet)), alphabet))
 
 
 def encrypt (message, key):
+
+    """Encodes the message by using the key to index each character."""
+
     encrypted = ''
-    #split the message to the length of the key
-    #(start, end, step)
     split_message = [message[i:i + len(key)] for i in range(0, len(message), len(key))]
     #convert message to index and add the key
     for each_split in split_message:
-        i = 0
+        q = 0
         for letter in each_split:
-            number = (letter_to_index[letter] + letter_to_index[key[i]]) % len(alphabet)
-            encrypted += index_to_letter[number]
-            i +=1
+            number = (letter_then_index[letter] + letter_then_index[key[q]]) % len(alphabet)
+            encrypted += index_then_letter[number]
+            q +=1
 
     # write encrpted message text
 
     return encrypted
 
 def decrypt(cipher, key):
+
+    """Decodes the cipher using the key"""
     decrypted = ''
 
     # split the cipher to the length of the key
-    split_cipher = [cipher [i:i + len(key)] for i in range(0, len(cipher), len(key))]
+    split_cipher = [cipher [k:k + len(key)] for k in range(0, len(cipher), len(key))]
     for each_split in split_cipher:
-        i = 0
+        j = 0
         for letter in each_split:
-            number = (letter_to_index[letter] - letter_to_index[key[i]]) % len(alphabet)
-            decrypted += index_to_letter[number]
-            i += 1
+            number = (letter_then_index[letter] - letter_then_index[key[j]]) % len(alphabet)
+            decrypted += index_then_letter[number]
+            j += 1
 
 
     # write decrypted message text
@@ -40,7 +43,10 @@ def decrypt(cipher, key):
     return decrypted
 
 def main():
-    key = 'bananasa'
+
+    """Main Program to run the program so the user may interact with it."""
+
+    key = 'pineappleavocado'
     message = input("Enter message to be encryped: ")
     encrypted_message = encrypt(message, key)
     decrypted_message = decrypt(encrypted_message, key)
@@ -51,10 +57,3 @@ def main():
 
 main()
 
-
-
-
-
-""" Youtube tutorial video:
-    https://www.youtube.com/watch?v=sxFObRNriUg&ab_channel=AladdinPersson
-    Current location- 3:15"""
